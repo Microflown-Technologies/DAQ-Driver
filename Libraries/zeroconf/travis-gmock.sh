d0a1e485424b8ab7d9f9c592cf27c44471963380
Benjamin Martens <benjamin@MF-WS-018-Linux.microflown.loc>
2020-07-01 09:29:12 +0200

    Removed Library files and replaced with submodule, removed .hgignore file left from migration from mercurial to git

--- Libraries/zeroconf/travis-gmock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir google-mock
-wget https://github.com/google/googletest/archive/release-1.8.0.tar.gz
-tar xf release-1.8.0.tar.gz
-pushd googletest-release-1.8.0
-cmake -DCMAKE_INSTALL_PREFIX:PATH=./ .
-make && make install
-popd
